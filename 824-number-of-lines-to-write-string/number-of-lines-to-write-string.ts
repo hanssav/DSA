ff46ee262f82f3806b97673a97a01ead06c6b260
hanssav <63248102+hanssav@users.noreply.github.com>
2026-01-01 21:04:39 +0700

    Time: 127 ms (10.00%) | Memory: 64.9 MB (10.00%) - LeetSync

---
+++ 824-number-of-lines-to-write-string/number-of-lines-to-write-string.ts
@@ -0,0 +1,20 @@
+function numberOfLines(widths: number[], s: string): number[] { 
+    let lines = 1; 
+    let curr_widths = 0; 
+
+    for(let i = 0; i < s.length; i++){
+        let word = s[i]; 
+        //find index use ASCII, a = 97, b = 98, c = 99 (a=0, b=1, c=2...)
+        let idx = word.charCodeAt(0) - 97; 
+
+        curr_widths += widths[idx]; 
+        
+        if(!!s[i + 1] && curr_widths + widths[s[i + 1].charCodeAt(0) - 97] > 100){
+            lines++
+            curr_widths = 0
+        }
+        console.log(curr_widths, 'curr')
+        console.log(word, idx, s[i])
+    }
+    return [lines, curr_widths]
+};
