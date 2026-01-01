2efd4ded015edf50e9e196e46d27969ae0711ef7
hanssav <63248102+hanssav@users.noreply.github.com>
2026-01-01 13:45:09 +0700

    Time: 92 ms (65.12%) | Memory: 86.4 MB (9.30%) - LeetSync

--- 310-minimum-height-trees/minimum-height-trees.ts
+++ 310-minimum-height-trees/minimum-height-trees.ts
@@ -1,7 +1,10 @@
 function findMinHeightTrees(n: number, edges: number[][]): number[] {
     if (n === 1) return [0];
     
+    // n = 6, edges = [[3,0],[3,1],[3,2],[3,4],[5,4]]
+    // adj data to see neighbor; 
     const m = new Map(); 
+    //count neighbor data; 
     const degree = new Array(n).fill(0);
 
     for(let [u, v] of edges){
@@ -15,6 +18,7 @@ function findMinHeightTrees(n: number, edges: number[][]): number[] {
         degree[v]++;
     }
 
+    //find singgle leaves/ outside leave
     let leaves: number[] = []
     for(let i = 0; i < n; i++){
         if(degree[i] === 1){
@@ -22,25 +26,30 @@ function findMinHeightTrees(n: number, edges: number[][]): number[] {
         }
     }
 
+    // 
     let total_nodes = n; 
-
     while(total_nodes > 2){
-        total_nodes -= leaves.length; 
+        // delete 4 leave (0, 1, 2, 5)
+        total_nodes -= leaves.length; // 6 - 4 = 2 (Sisa 2 node) 
 
         const new_leaves: number[] = []
 
         for(let leaf of leaves){
+            // Ambil tetangga dari si daun yang mau dihapus
             const neighbors = m.get(leaf) || []; 
 
             for(let neighbor of neighbors){
+                // Karena 'leaf' dihapus, maka koneksi si tetangga berkurang 1
                 degree[neighbor]--;
+                // JIKA tetangga itu sekarang cuma punya 1 koneksi, 
+                // dia resmi jadi daun baru untuk ronde berikutnya
                 if(degree[neighbor] === 1){
                     new_leaves.push(neighbor); 
                 }
             }
         }
-
-        leaves = new_leaves
+        // Update daftar daun untuk iterasi berikutnya
+        leaves = new_leaves // leaves sekarang jadi [3, 4]
     }
     return leaves
 };
