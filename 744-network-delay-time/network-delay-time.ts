d84e21e41a6a5e742cd8d430d0c94bae3b1c9cd0
hanssav <63248102+hanssav@users.noreply.github.com>
2026-01-02 11:49:47 +0700

    Time: 106 ms (69.63%) | Memory: 67.6 MB (61.26%) - LeetSync

--- 744-network-delay-time/network-delay-time.ts
+++ 744-network-delay-time/network-delay-time.ts
@@ -7,17 +7,25 @@ function networkDelayTime(times: number[][], n: number, k: number): number {
         adj_list.get(src).push([target, time])
     }
 
+    // console.log(adj_list, 'adj list'); 
+    
     const queue = [ k ]; 
     const min_times = new Array(n + 1).fill(Infinity); 
     min_times[k] = 0;
 
+    // console.log(min_times, 'min_times 1'); 
+
     while(queue.length > 0){
         const curr = queue.shift(); 
 
+        // console.log(curr, 'curr')
         if(adj_list.has(curr)){
             for(let [target, travel_time] of adj_list.get(curr)!){
                 const new_time = min_times[curr] + travel_time; 
-                
+                // console.log(new_time, travel_time, min_times[curr], curr, 'curr loop')
+
+                // console.log(new_time,min_times[target],  'target new time', new_time < min_times[target]); 
+                // console.log("----")
                 if(new_time < min_times[target]) {
                     min_times[target] = new_time; 
                     queue.push(target) 
@@ -26,13 +34,19 @@ function networkDelayTime(times: number[][], n: number, k: number): number {
         } 
     }
     
+    // console.log(min_times, 'min_times'); 
+
     let max_delay = 0; 
+    
+    // console.log(max_delay, 'max_ delay before'); 
 
     for(let i = 1; i <= n; i++){
         if(min_times[i] === Infinity) return -1; 
 
         max_delay = Math.max(max_delay, min_times[i]); 
     }
-    
+
+    // console.log(max_delay, 'max_ delay after')
+
     return max_delay; 
 };
