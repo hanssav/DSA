1df2c3830943ba2b49705ade063fc573b5812b31
hanssav <63248102+hanssav@users.noreply.github.com>
2026-01-03 12:48:25 +0700

    Time: 21 ms (5.26%) | Memory: 61.4 MB (7.89%) - LeetSync

---
+++ 979-di-string-match/di-string-match.ts
@@ -0,0 +1,18 @@
+function diStringMatch(s: string): number[] {
+    const nums: number[] = Array.from({length: s.length + 1}, (_, i) => i) 
+    const res: number[] = []
+
+    console.log(nums, 'nums')
+    for(let str of s){
+        let num = 0; 
+
+        if(str === "I") num = nums.shift()
+        else num = nums.pop()
+
+        res.push(num)
+    }
+
+    res.push(nums.shift())
+
+    return res
+};
