59bf6ceec6e19dfec7481a5d9ca05c64ee71ffd9
hanssav <63248102+hanssav@users.noreply.github.com>
2026-01-03 11:06:23 +0700

    Time: 76 ms (0.00%) | Memory: 66.1 MB (6.45%) - LeetSync

---
+++ 961-long-pressed-name/long-pressed-name.ts
@@ -0,0 +1,21 @@
+function isLongPressedName(name: string, typed: string): boolean {
+    let i = 0; 
+    let j = 0; 
+
+    while(i < name.length || j < typed.length){
+        console.log(i, j, name[i], typed[j])
+
+        if(name[i] === typed[j]) {
+            i++; 
+            j++; 
+        } else if(name[i] !== typed[j] && name[i - 1] === typed[j]){
+            j++; 
+        } else if(name[i] !== typed[j] && name[i - 1] !== typed[j]){
+            return false
+        }
+    }
+
+    console.log(j === typed.length, i === name.length, 'res', false === false)
+    console.log(i === name.length - 1 && j === typed.length - 1 , 'check condition')
+    return i === name.length && j === typed.length ? true : false 
+};
