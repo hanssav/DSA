f7cc9547ad98fcdae517e0273402b02a5f48e89f
hanssav <63248102+hanssav@users.noreply.github.com>
2026-01-07 10:50:29 +0700

    Time: 3 ms (38.71%) | Memory: 57.6 MB (56.45%) - LeetSync

--- 1547-destination-city/destination-city.ts
+++ 1547-destination-city/destination-city.ts
@@ -1,16 +1,16 @@
 function destCity(paths: string[][]): string {
     const sources = new Set<string>();  
-    const all_cities = new Set<string>(); 
+    // const all_cities = new Set<string>(); 
 
-    for(let [src, dest] of paths){
+    for(let [src, _] of paths){
         sources.add(src);
-        all_cities.add(src); 
-        all_cities.add(dest);  
+        // all_cities.add(src); 
+        // all_cities.add(dest);  
     }
 
-    for(let city of all_cities){
+    for(let [_, dest] of paths){
         // console.log(typeof city, sources.has(city))
-        if(!sources.has(city)) return city; 
+        if(!sources.has(dest)) return dest; 
     }
 
     return ""
