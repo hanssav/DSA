a584877d13a22918529be2c0ab80a09e51c2a6aa
hanssav <63248102+hanssav@users.noreply.github.com>
2026-01-08 14:31:15 +0700

    Time: 4 ms (15.00%) | Memory: 59.8 MB (7.50%) - LeetSync

---
+++ 1651-shuffle-string/shuffle-string.ts
@@ -0,0 +1,12 @@
+function restoreString(s: string, indices: number[]): string {
+    const res = ""
+    const len = s.length
+
+    const pair = []
+
+    for(let i = 0; i < len; i++){
+        pair.push([indices[i], s[i]]); 
+    }
+
+    return pair.sort((a,b) => a[0] - b[0]).map(item => item[1]).join("")
+};
