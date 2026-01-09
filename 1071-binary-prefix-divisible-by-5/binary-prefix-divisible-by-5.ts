95424e903aa235664263c4c303cc207f9757dc29
hanssav <63248102+hanssav@users.noreply.github.com>
2026-01-09 10:10:18 +0700

    Time: 4 ms (53.52%) | Memory: 60.5 MB (44.18%) - LeetSync

--- 1071-binary-prefix-divisible-by-5/binary-prefix-divisible-by-5.ts
+++ 1071-binary-prefix-divisible-by-5/binary-prefix-divisible-by-5.ts
@@ -3,8 +3,10 @@ function prefixesDivBy5(nums: number[]): boolean[] {
     let res = []
     for(let i = 0; i < nums.length; i++){
         curr = (curr * 2 + nums[i]) % 5
-        if(curr === 0) res.push(true); 
-        else res.push(false)
+        
+        res.push(curr === 0);
+        // if(curr === 0) res.push(true); 
+        // else res.push(false)
     }
 
     return res
