cd5ba8a21ac77e80b964d96fd9d0004dbeb4ed3f
hanssav <63248102+hanssav@users.noreply.github.com>
2026-01-09 11:46:02 +0700

    Time: 41 ms (5.88%) | Memory: 65.4 MB (5.88%) - LeetSync

---
+++ 1386-shift-2d-grid/shift-2d-grid.ts
@@ -0,0 +1,18 @@
+function shiftGrid(grid: number[][], k: number): number[][] {
+    const flat = grid.flat()
+
+    for(let i = 0; i < k; i++){
+        // const last = flat.pop() 
+        flat.unshift(flat.pop());
+    }
+
+    const res = []
+    const cols = grid[0].length; 
+
+    for(let i = 0; i < flat.length; i += cols){
+        res.push(flat.slice(i, i + cols))
+    } 
+    
+    console.log(flat, res)
+    return res
+};
