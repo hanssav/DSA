c5be45df3ca2d9ba9271afe9fb8cc5844afbfa3e
hanssav <63248102+hanssav@users.noreply.github.com>
2026-01-14 18:02:59 +0700

    Time: 37 ms (56.44%) | Memory: 73.2 MB (62.15%) - LeetSync

---
+++ 15-3sum/3sum.ts
@@ -0,0 +1,30 @@
+function threeSum(nums: number[]): number[][] {
+    nums.sort((a, b) => a - b); 
+
+    const res:number[][] = [];
+
+    for(let i = 0; i < nums.length - 2; i++){
+        if(i > 0 && nums[i] === nums[i - 1]) continue; 
+
+        let left = i + 1;
+        let right = nums.length - 1; 
+
+        while(right > left){
+            let sum = nums[i] + nums[right] + nums[left]; 
+
+            if(sum < 0) left++; 
+            else if(sum > 0) right--;
+            else{
+                res.push([nums[i], nums[right], nums[left]])
+                // 2. Skip angka 'left' yang sama
+                while (left < right && nums[left] === nums[left + 1]) left++;
+                // 3. Skip angka 'right' yang sama
+                while (left < right && nums[right] === nums[right - 1]) right--;
+                
+                left++;
+                right--;
+            }
+        }
+    }
+    return res;
+};
