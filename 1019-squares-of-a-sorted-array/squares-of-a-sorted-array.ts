8a6a5c9df9c31a6d96de152c3bfc1dd63ae7f49f
hanssav <63248102+hanssav@users.noreply.github.com>
2026-01-15 12:42:59 +0700

    Time: 8 ms (49.30%) | Memory: 62.5 MB (83.52%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.ts
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.ts
@@ -1,5 +1,7 @@
 function sortedSquares(nums: number[]): number[] {
-    for(let [i, num] of nums.entries()) nums[i] = Math.pow(num, 2)
-
+    // for(let [i, num] of nums.entries()) nums[i] = Math.pow(num, 2)
+    for(let i = 0; i < nums.length; i++){
+        nums[i] *= nums[i]
+    }
     return nums.sort((a, b) => a - b)
 };
