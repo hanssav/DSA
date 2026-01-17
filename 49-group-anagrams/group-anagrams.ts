236064c6f0a0132aa13b642e0647661bbb0e504b
hanssav <63248102+hanssav@users.noreply.github.com>
2026-01-17 17:45:54 +0700

    Time: 32 ms (54.03%) | Memory: 68.7 MB (50.12%) - LeetSync

--- 49-group-anagrams/group-anagrams.ts
+++ 49-group-anagrams/group-anagrams.ts
@@ -1,12 +1,28 @@
 function groupAnagrams(strs: string[]): string[][] {
     const res = new Map(); 
 
-    const normalize = (s) => s
-        .replace(/[^\w]/g, '')
-        .toLowerCase()
-        .split("")
-        .sort()
-        .join("")
+    // const normalize = (s) => s
+    //     .replace(/[^\w]/g, '')
+    //     .toLowerCase()
+    //     .split("")
+    //     .sort()
+    //     .join("")
+    
+    function normalize(s) {
+        let result = [];
+        for (let i = 0; i < s.length; i++) {
+            let code = s.charCodeAt(i);
+            // Cek apakah karakter adalah angka (48-57) atau huruf (97-122)
+            // Kita ubah ke lowercase dulu secara manual atau pakai toLowerCase()
+            let char = s[i].toLowerCase();
+            let charCode = char.charCodeAt(0);
+            
+            if ((charCode > 47 && charCode < 58) || (charCode > 96 && charCode < 123)) {
+                result.push(char);
+            }
+        }
+        return result.sort().join("");
+    }
 
     for(let str of strs){
         const normalized = normalize(str);
