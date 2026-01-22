ff4e15c26be430664ab80218403035edddacc58b
hanssav <63248102+hanssav@users.noreply.github.com>
2026-01-22 14:33:19 +0700

    Time: 36 ms (84.22%) | Memory: 55 MB (54.37%) - LeetSync

---
+++ 374-guess-number-higher-or-lower/guess-number-higher-or-lower.ts
@@ -0,0 +1,25 @@
+/** 
+ * Forward declaration of guess API.
+ * @param {number} num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * var guess = function(num) {}
+ */
+
+
+function guessNumber(n: number): number {
+    let low = 1; 
+    let high = n; 
+
+    while(high >= low){
+        const mid = low + Math.floor((high - low) / 2); 
+        const res = guess(mid); 
+
+        if(res === 0) return mid; 
+        else if(res === -1) high = mid - 1; 
+        else low = mid + 1; 
+    }
+
+    return -1;
+};
