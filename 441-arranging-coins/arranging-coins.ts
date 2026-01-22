7cdc7cf7a45c732e5b809d0246d5a463d54d7fb3
hanssav <63248102+hanssav@users.noreply.github.com>
2026-01-22 15:02:47 +0700

    Time: 2 ms (58.25%) | Memory: 58.2 MB (41.75%) - LeetSync

--- 441-arranging-coins/arranging-coins.ts
+++ 441-arranging-coins/arranging-coins.ts
@@ -1,10 +1,22 @@
+/* 
+    Rumus: 
+    S= (k * (k+1)) / 2
+​   
+    Manual: 1+2+3+4+5=15 koin.
+    Rumus = (5 * (5 + 1)) / 2
+*/ 
 function arrangeCoins(n: number): number {
-    let row: number = 1;
+    let low = 0; 
+    let high = n;
 
-    while(n > 0){
-        n -= row;
-        if(n > row) row++;
+    while(high >= low){
+        let mid = Math.floor(low + ((high - low) / 2)); 
+        let need = (mid * (mid + 1)) / 2
+
+        if(need === n) return mid; 
+        else if(need < n) low = mid + 1;
+        else high = mid - 1;
     }
 
-    return row;
+    return high;
 };
