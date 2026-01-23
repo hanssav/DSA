85500aec84b99250f3f200f31f0b4cbf50104e79
hanssav <63248102+hanssav@users.noreply.github.com>
2026-01-23 11:26:55 +0700

    Time: 0 ms (100.00%) | Memory: 55.2 MB (71.91%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.ts
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.ts
@@ -1,3 +1,22 @@
+// function findMin(nums: number[]): number {
+//     return Math.min(...nums)
+// };
+
+
 function findMin(nums: number[]): number {
-    return Math.min(...nums)
+    let left = 0; 
+    let right = nums.length - 1; 
+    let min = Infinity;
+
+    while(right >= left){
+        let mid = Math.floor((left + right) / 2); 
+
+        if(nums[mid] < min) min = nums[mid]
+
+        if(Math.min(nums[mid], nums[left]) > Math.min(nums[mid], nums[right]) ){
+            left = mid + 1; 
+        } else right = mid - 1;
+    }
+
+    return min;
 };
