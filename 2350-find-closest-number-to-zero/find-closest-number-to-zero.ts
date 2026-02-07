95f3b4287ee09d0d52ce8dcef094d88d5b217122
Handi Irawan <63248102+hanssav@users.noreply.github.com>
2026-02-07 12:10:25 +0700

    Time: 0 ms (100.00%) | Memory: 58.3 MB (78.48%) - LeetSync

--- 2350-find-closest-number-to-zero/find-closest-number-to-zero.ts
+++ 2350-find-closest-number-to-zero/find-closest-number-to-zero.ts
@@ -1,14 +1,17 @@
 function findClosestNumber(nums: number[]): number {
     let closest = nums[0];
-
-    for(let i = 1; i < nums.length; i++){
-        let diff = Math.abs(nums[i])
-        if(Math.abs(closest) > diff){
-            closest = nums[i];
-        } else if(Math.abs(closest) === diff && nums[i] > closest){
-            closest = nums[i]
+    
+    for(let i = 1; i < nums.length; i++) {
+        const current = nums[i];
+        const absCurrent = Math.abs(current);
+        const absClosest = Math.abs(closest);
+        
+        if(absClosest > absCurrent) {
+            closest = current;
+        } else if(absClosest === absCurrent && current > closest) {
+            closest = current;
         }
     }
-
+    
     return closest;
 };
