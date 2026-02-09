3e0cda3090eea90bf57c1f3587e3129ddbd41a9a
Handi Irawan <63248102+hanssav@users.noreply.github.com>
2026-02-09 13:40:36 +0700

    Time: 7 ms (39.29%) | Memory: 57.3 MB (62.50%) - LeetSync

--- 137-single-number-ii/single-number-ii.ts
+++ 137-single-number-ii/single-number-ii.ts
@@ -1,9 +1,9 @@
 function singleNumber(nums: number[]): number {
     const m = new Map<number, number>(); 
 
-    for(const num of nums){
-        m.set(num, (m.get(num) || 0) + 1);
-        if(m.get(num) >= 3) m.delete(num); 
+    for(let i = 0; i < nums.length; i++){
+        m.set(nums[i], (m.get(nums[i]) || 0) + 1);
+        if(m.get(nums[i]) >= 3) m.delete(nums[i]); 
     }
     
     const [first] = m.keys();
