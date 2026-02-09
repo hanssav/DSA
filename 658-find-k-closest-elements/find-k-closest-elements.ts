5420bfc25152aee35fedfa88e684428bbf888bbd
Handi Irawan <63248102+hanssav@users.noreply.github.com>
2026-02-09 09:01:08 +0700

    Time: 272 ms (5.51%) | Memory: 67.2 MB (7.87%) - LeetSync

---
+++ 658-find-k-closest-elements/find-k-closest-elements.ts
@@ -0,0 +1,15 @@
+function findClosestElements(arr: number[], k: number, x: number): number[] {
+    let l = 0; 
+    let r = arr.length - 1;
+
+    while(r - l + 1 > k){
+        let disL = Math.abs(arr[l] - x); 
+        let disR = Math.abs(arr[r] - x); 
+        
+        if(disL === disR || disR > disL) r--;
+        else l++; 
+        console.log(l, r)
+    }
+
+    return arr.slice(l, r + 1);
+};
