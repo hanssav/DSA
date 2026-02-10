f3e1d878f3cd7857958c83a414eeb8303cbe7730
Handi Irawan <63248102+hanssav@users.noreply.github.com>
2026-02-10 21:04:48 +0700

    Time: 2 ms (45.83%) | Memory: 64.6 MB (86.81%) - LeetSync

--- 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.ts
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.ts
@@ -1,11 +1,10 @@
 function replaceElements(arr: number[]): number[] {
     let max = -1;
     
-    // Iterasi dari kanan ke kiri
     for (let i = arr.length - 1; i >= 0; i--) {
-        const current = arr[i];  // Simpan nilai asli
-        arr[i] = max;           // Ganti dengan max sebelah kanan
-        max = Math.max(max, current); // Update max
+        const current = arr[i]; 
+        arr[i] = max;      
+        max = Math.max(max, current);
     }
     
     return arr;
