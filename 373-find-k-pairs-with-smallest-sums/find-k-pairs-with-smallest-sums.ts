413d2a12f3cf851a60c34b9e9d0bbfe635345349
Handi Irawan <63248102+hanssav@users.noreply.github.com>
2026-02-10 14:12:56 +0700

    Time: 73 ms (45.45%) | Memory: 87.8 MB (37.12%) - LeetSync

--- 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.ts
+++ 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.ts
@@ -1,34 +1,79 @@
-function kSmallestPairs(nums1: number[], nums2: number[], k: number): number[][] {
-    const res: number[][] = [];
-    if (!nums1.length || !nums2.length || k === 0) return res;
+class MyMinHeap {
+    private heap: [number, number, number][];
 
-    // "Queue" berisi: [totalSum, indexNums1, indexNums2]
-    const queue: [number, number, number][] = [];
-
-    // Inisialisasi: Masukkan pasangan awal (nums1[i], nums2[0])
-    for (let i = 0; i < Math.min(nums1.length, k); i++) {
-        queue.push([nums1[i] + nums2[0], i, 0]);
+    constructor() {
+        this.heap = [];
     }
 
-    while (res.length < k && queue.length > 0) {
-        // Ambil yang terkecil (elemen pertama)
-        const [sum, i, j] = queue.shift()!;
+    size(): number {
+        return this.heap.length;
+    }
+
+    push(val: [number, number, number]): void {
+        this.heap.push(val);
+        this.bubbleUp(this.heap.length - 1);
+    }
+
+    pop(): [number, number, number] | undefined {
+        if (this.size() === 0) return undefined;
+        if (this.size() === 1) return this.heap.pop();
+
+        const top = this.heap[0];
+        this.heap[0] = this.heap.pop()!;
+        this.bubbleDown(0);
+        return top;
+    }
+
+    private bubbleUp(index: number): void {
+        while (index > 0) {
+            let parent = Math.floor((index - 1) / 2);
+            if (this.heap[parent][0] <= this.heap[index][0]) break;
+            [this.heap[parent], this.heap[index]] = [this.heap[index], this.heap[parent]];
+            index = parent;
+        }
+    }
+
+    private bubbleDown(index: number): void {
+        while (true) {
+            let left = 2 * index + 1;
+            let right = 2 * index + 2;
+            let smallest = index;
+
+            if (left < this.heap.length && this.heap[left][0] < this.heap[smallest][0]) {
+                smallest = left;
+            }
+            if (right < this.heap.length && this.heap[right][0] < this.heap[smallest][0]) {
+                smallest = right;
+            }
+
+            if (smallest === index) break;
+
+            [this.heap[index], this.heap[smallest]] = [this.heap[smallest], this.heap[index]];
+            index = smallest;
+        }
+    }
+}
+
+function kSmallestPairs(nums1: number[], nums2: number[], k: number): number[][] {
+    const res: number[][] = [];
+    if (!nums1.length || !nums2.length || k <= 0) return res;
+
+    // Gunakan nama class yang baru
+    const minHeap = new MyMinHeap();
+
+    for (let i = 0; i < Math.min(nums1.length, k); i++) {
+        minHeap.push([nums1[i] + nums2[0], i, 0]);
+    }
+
+    while (res.length < k && minHeap.size() > 0) {
+        const item = minHeap.pop();
+        if (!item) break;
+
+        const [_, i, j] = item;
         res.push([nums1[i], nums2[j]]);
 
-        // Jika masih ada elemen di nums2, masukkan pasangan berikutnya
         if (j + 1 < nums2.length) {
-            const nextSum = nums1[i] + nums2[j + 1];
-            const nextPair: [number, number, number] = [nextSum, i, j + 1];
-            
-            // CARA MUDAH: Masukkan ke posisi yang tepat agar queue tetap terurut
-            // Ini menggunakan teknik Binary Search sederhana (insertion sort style)
-            let low = 0, high = queue.length;
-            while (low < high) {
-                let mid = (low + high) >>> 1;
-                if (queue[mid][0] < nextSum) low = mid + 1;
-                else high = mid;
-            }
-            queue.splice(low, 0, nextPair);
+            minHeap.push([nums1[i] + nums2[j + 1], i, j + 1]);
         }
     }
 
