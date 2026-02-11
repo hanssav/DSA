a7794965e7930df2784864cfd65c76dc762e1792
Handi Irawan <63248102+hanssav@users.noreply.github.com>
2026-02-11 08:51:59 +0700

    Time: 17 ms (44.26%) | Memory: 62.6 MB (42.62%) - LeetSync

--- 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.ts
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.ts
@@ -1,14 +1,10 @@
+function isPalindromic(str: string): boolean {
+    const reversedStr = str.split("").reverse().join(""); 
+    return str === reversedStr;
+}
+
+
 function firstPalindrome(words: string[]): string {
-
-    function isPalindromic(str: string): boolean {
-        // Normalize string: convert to lower case and remove non-alphanumeric characters
-        const cleanStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
-
-        const reversedStr = cleanStr.split("").reverse().join(""); 
-
-        return cleanStr === reversedStr;
-    }
-
     for(let word of words){
         if(isPalindromic(word)) return word
     }
