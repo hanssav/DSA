291bd5e7a624e72f809b3b9ea2479d52be66a19e
Handi Irawan <63248102+hanssav@users.noreply.github.com>
2026-02-14 13:52:18 +0700

    Time: 9 ms (61.54%) | Memory: 58.5 MB (38.46%) - LeetSync

---
+++ 1500-count-largest-group/count-largest-group.ts
@@ -0,0 +1,26 @@
+function countLargestGroup(n: number): number {
+    let map = new Map<number, number>();
+    let largest = 0; 
+
+    for(let i = 1; i <= n; i++){
+        let num = i; 
+        let sum = 0; 
+
+        while(num > 0){
+            sum += num % 10; 
+            num = Math.floor(num / 10);
+        }
+
+        map.set(sum, (map.get(sum) || 0) + 1)
+        
+        largest = Math.max(largest, map.get(sum));
+    }
+
+    let res = 0;
+
+    for(const [_, count] of map) {
+        if(count === largest) res++;
+    }
+  
+    return res;
+};
