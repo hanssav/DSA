9dcf33f4e5d2f719802b933728fbc728eabdca67
Handi Irawan <63248102+hanssav@users.noreply.github.com>
2026-02-15 11:19:28 +0700

    Time: 32 ms (9.09%) | Memory: 64.3 MB (9.09%) - LeetSync

---
+++ 4053-majority-frequency-characters/majority-frequency-characters.ts
@@ -0,0 +1,31 @@
+function majorityFrequencyGroup(s: string): string {
+    const map = new Map<string, number>();
+
+    for(const str of s){
+        map.set(str, (map.get(str) || 0) + 1);
+    };
+
+    const groupMap = new Map<number, string[]>(); 
+
+    for(const [ch, freq] of map){
+        if(!groupMap.has(freq)) groupMap.set(freq, []); 
+
+        groupMap.get(freq)!.push(ch);
+    }; 
+
+    let maxSize = 0; 
+    let bestFreq = 0; 
+
+    console.log(groupMap)
+    for(const [freq, chars] of groupMap){
+        if(
+            chars.length > maxSize ||
+            (chars.length === maxSize && freq > bestFreq) 
+        ) {
+            maxSize = chars.length; 
+            bestFreq = freq;
+        }
+    }
+
+    return groupMap.get(bestFreq)!.join("")
+};
