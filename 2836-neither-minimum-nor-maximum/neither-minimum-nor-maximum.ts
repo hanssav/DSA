dcc4671002073829ae9058cfd6b1d90ec5033c80
Handi Irawan <63248102+hanssav@users.noreply.github.com>
2026-02-17 11:31:51 +0700

    Time: 4 ms (100.00%) | Memory: 66.7 MB (47.22%) - LeetSync

--- 2836-neither-minimum-nor-maximum/neither-minimum-nor-maximum.ts
+++ 2836-neither-minimum-nor-maximum/neither-minimum-nor-maximum.ts
@@ -1,27 +1,27 @@
-// function findNonMinOrMax(nums: number[]): number {
-//     let min = Infinity; 
-//     let max = -Infinity; 
-
-//     for(const num of nums){
-//         if(num < min) min = num; 
-//         if(num > max) max = num;
-//     }; 
-
-//     for(const num of nums){
-//         if(num !== min && num !== max) return num; 
-//     }; 
-
-//     return -1;
-// };
-
 function findNonMinOrMax(nums: number[]): number {
-    if(nums.length < 2) return -1; 
-
-    let min = Math.min(...nums); 
-    let max = Math.max(...nums); 
+    let min = Infinity; 
+    let max = -Infinity; 
 
     for(const num of nums){
-        if(min !== num && max !== num) return num
-    }
+        if(num < min) min = num; 
+        if(num > max) max = num;
+    }; 
+
+    for(const num of nums){
+        if(num !== min && num !== max) return num; 
+    }; 
+
     return -1;
 };
+
+// function findNonMinOrMax(nums: number[]): number {
+//     if(nums.length < 2) return -1; 
+
+//     let min = Math.min(...nums); 
+//     let max = Math.max(...nums); 
+
+//     for(const num of nums){
+//         if(min !== num && max !== num) return num
+//     }
+//     return -1;
+// };
