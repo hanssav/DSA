05801828f3abf0f7eb8249ac813eb86319c2a920
Handi Irawan <63248102+hanssav@users.noreply.github.com>
2026-02-19 13:05:39 +0700

    Time: 27 ms (9.09%) | Memory: 65.3 MB (59.09%) - LeetSync

--- 2825-minimize-string-length/minimize-string-length.ts
+++ 2825-minimize-string-length/minimize-string-length.ts
@@ -1,4 +1,7 @@
 function minimizedStringLength(s: string): number {
-    const set = new Set<string>([...s.split("")])
+    const set = new Set<string>()
+    for(const str of s){
+        set.add(str);
+    }
     return set.size;
 };
