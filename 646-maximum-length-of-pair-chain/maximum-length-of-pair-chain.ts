3be5034c00dc46134e1db1a21fadf6af6fb01a05
Handi Irawan <63248102+hanssav@users.noreply.github.com>
2026-02-21 10:35:08 +0700

    Time: 90 ms (6.67%) | Memory: 61.2 MB (10.00%) - LeetSync

--- 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.ts
+++ 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.ts
@@ -2,16 +2,28 @@ function findLongestChain(pairs: number[][]): number {
     pairs.sort((a, b) => a[0] - b[0]);
     
     const n = pairs.length;
+    const memo = new Array(n).fill(0); 
+    
+    function solve(i: number): number{
+        if(memo[i] !== 0) return memo[i]
+ 
+        let res = 1; 
 
-    const dp = new Array(n).fill(1); 
-
-    for(let i = 0; i < n; i++){
         for(let j = 0; j < i; j++){
             if(pairs[j][1] < pairs[i][0]){
-                dp[i] = Math.max(dp[i], dp[j] + 1);
+                res = Math.max(res, solve(j) + 1);
             }
         }
-    }; 
 
-    return Math.max(...dp)
+        memo[i] = res; 
+        return res;
+    }
+
+    let maxChain = 0; 
+
+    for(let i = 0; i < n; i++){
+        maxChain = Math.max(maxChain, solve(i))
+    }
+
+    return maxChain;
 };
