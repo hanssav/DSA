cd9c9c41729e6366c3c9c454ffd814033a5fb56a
Handi Irawan <63248102+hanssav@users.noreply.github.com>
2026-03-10 23:43:09 +0700

    Time: 1 ms (55.56%) | Memory: 58 MB (48.15%) - LeetSync

---
+++ 3429-special-array-i/special-array-i.ts
@@ -0,0 +1,9 @@
+function isArraySpecial(nums: number[]): boolean {
+    if(nums.length <= 1)return true; 
+
+    for(let i = 0; i < nums.length - 1; i++){
+        if(nums[i] % 2 === nums[i + 1] % 2) return false
+    }
+
+    return true;
+};
