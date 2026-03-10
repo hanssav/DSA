a6267aedbe9d4baf693db9082cb43a285997c379
Handi Irawan <63248102+hanssav@users.noreply.github.com>
2026-03-10 23:44:50 +0700

    Time: 55 ms (35.48%) | Memory: 57 MB (45.16%) - LeetSync

---
+++ 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.ts
@@ -0,0 +1,22 @@
+function countValidSelections(nums: number[]): number {
+    const totalSum = nums.reduce((a, b) => a + b, 0);
+    let runningSum = 0;
+    let count = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === 0) {
+            const left = runningSum;
+            const right = totalSum - runningSum;
+
+            if (left === right) {
+                count += 2;
+            } else if (Math.abs(left - right) === 1) {
+                count += 1;
+            }
+        } else {
+            runningSum += nums[i];
+        }
+    }
+
+    return count;
+}
