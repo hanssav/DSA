15119eb04c46fa3851f3dd489a45e47b66607cbc
Handi Irawan <63248102+hanssav@users.noreply.github.com>
2026-03-11 12:57:04 +0700

    Time: 3 ms (92.13%) | Memory: 70.2 MB (68.59%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.ts
@@ -0,0 +1,19 @@
+function productExceptSelf(nums: number[]): number[] {
+    const n = nums.length; 
+    const res: number[] = new Array(n).fill(1); 
+
+    let leftProduct = 1; 
+
+    for(let i = 0; i < n; i++){
+        res[i] = leftProduct
+        leftProduct *= nums[i];
+    }
+
+    let rightProduct = 1;
+    for (let i = n - 1; i >= 0; i--) {
+        res[i] *= rightProduct;
+        rightProduct *= nums[i];
+    }
+
+    return res;
+};  
