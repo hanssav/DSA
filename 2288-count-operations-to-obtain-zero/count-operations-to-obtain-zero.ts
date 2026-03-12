45f02bd907f3ad4855784d22e238db6190c6c308
Handi Irawan <63248102+hanssav@users.noreply.github.com>
2026-03-12 19:55:16 +0700

    Time: 3 ms (53.49%) | Memory: 54.2 MB (86.05%) - LeetSync

---
+++ 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.ts
@@ -0,0 +1,15 @@
+function countOperations(num1: number, num2: number): number {
+    let act = 0; 
+
+    while(num1 > 0 && num2 > 0){
+        if(num1 >= num2){
+            num1 -= num2
+        } else {
+            num2 -= num1
+        }
+
+        act++
+    }
+
+    return act
+};
