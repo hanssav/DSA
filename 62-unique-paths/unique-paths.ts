49e023f32f89a3048ed954c729e2e091f434f8d9
Handi Irawan <63248102+hanssav@users.noreply.github.com>
2026-03-13 09:24:44 +0700

    Time: 499 ms (5.87%) | Memory: 66.2 MB (6.23%) - LeetSync

--- 62-unique-paths/unique-paths.ts
+++ 62-unique-paths/unique-paths.ts
@@ -1,16 +1,36 @@
+// function uniquePaths(m: number, n: number): number {
+//     const dp: number[] = Array(n).fill(1); 
+
+//     for(let i = 1; i < m; i++){
+//         for(let j = 1; j < n; j++){
+//             // dp[j] yang lama = sel di atas (i-1, j)
+//             // dp[j-1] yang baru = sel di kiri (i, j-1)
+
+//             dp[j] = dp[j] + dp[j - 1];
+
+//             console.log(dp)
+//         }
+//         console.log("=====")
+//     }
+
+//     return dp[n - 1];
+// };
+
 function uniquePaths(m: number, n: number): number {
-    const dp: number[] = Array(n).fill(1); 
+    const dp: number[][] = Array(m).fill(0).map(() => Array(n).fill(1)); 
 
-    for(let i = 1; i < m; i++){
-        for(let j = 1; j < n; j++){
-            // dp[j] yang lama = sel di atas (i-1, j)
-            // dp[j-1] yang baru = sel di kiri (i, j-1)
+    console.log(dp, 'dp')
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            // Cara mencapai (i,j) = cara dari atas + cara dari kiri
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
 
-            dp[j] = dp[j] + dp[j - 1];
-
-            // console.log(dp)
+            console.log(dp)
         }
+        console.log("====")
+
     }
 
-    return dp[n - 1];
-};
+    // Hasil akhir ada di pojok kanan bawah
+    return dp[m - 1][n - 1];
+}
