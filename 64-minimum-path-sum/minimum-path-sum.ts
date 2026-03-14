89a780c39118f181d42b573d02e4fc554f669f84
Handi Irawan <63248102+hanssav@users.noreply.github.com>
2026-03-14 22:21:45 +0700

    Time: 2 ms (92.48%) | Memory: 56.6 MB (94.67%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.ts
+++ 64-minimum-path-sum/minimum-path-sum.ts
@@ -38,20 +38,38 @@
 //     return calculate(grid.length - 1, grid[0].length - 1);
 // }
 
+// function minPathSum(grid: number[][]): number {
+//     const m = grid.length;
+//     const n = grid[0].length;
+//     const memo: number[][] = Array.from({ length: m }, () => Array(n).fill(-1));
+
+//     function solve(r: number, c: number): number {
+//         if (r === 0 && c === 0) return grid[0][0];
+//         if (r < 0 || c < 0) return Infinity;
+//         if (memo[r][c] !== -1) return memo[r][c];
+
+//         const res = grid[r][c] + Math.min(solve(r - 1, c), solve(r, c - 1));
+//         memo[r][c] = res;
+//         return res;
+//     }
+
+//     return solve(m - 1, n - 1);
+// }
+
 function minPathSum(grid: number[][]): number {
     const m = grid.length;
     const n = grid[0].length;
-    const memo: number[][] = Array.from({ length: m }, () => Array(n).fill(-1));
 
-    function solve(r: number, c: number): number {
-        if (r === 0 && c === 0) return grid[0][0];
-        if (r < 0 || c < 0) return Infinity;
-        if (memo[r][c] !== -1) return memo[r][c];
-
-        const res = grid[r][c] + Math.min(solve(r - 1, c), solve(r, c - 1));
-        memo[r][c] = res;
-        return res;
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (i === 0 && j === 0) continue;
+            
+            // Logika digabung untuk mengurangi jumlah pengecekan 'if'
+            const top = i > 0 ? grid[i - 1][j] : Infinity;
+            const left = j > 0 ? grid[i][j - 1] : Infinity;
+            
+            grid[i][j] += Math.min(top, left);
+        }
     }
-
-    return solve(m - 1, n - 1);
+    return grid[m - 1][n - 1];
 }
