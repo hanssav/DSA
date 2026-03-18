aeb9b1086fda421576be123bc18710f8eef03cc5
Handi Irawan <63248102+hanssav@users.noreply.github.com>
2026-03-18 22:31:10 +0700

    Time: 42 ms (98.34%) | Memory: 58.8 MB (27.29%) - LeetSync

--- 3651-transformed-array/transformed-array.ts
+++ 3651-transformed-array/transformed-array.ts
@@ -1,15 +1,32 @@
+// function constructTransformedArray(nums: number[]): number[] {
+//     const n = nums.length; 
+//     const res: number[] = new Array(n)
+
+//     for(let i = 0; i < n; i++){
+//         const val = nums[i];
+//         let idx = (i + val) % n;;
+
+//         if(idx < 0) idx += n
+
+//         res[i] = nums[idx]
+//     }
+
+//     return res;
+// };
+
 function constructTransformedArray(nums: number[]): number[] {
-    const n = nums.length; 
-    const res: number[] = new Array(n)
+    const n = nums.length;
+    const res = new Int32Array(n); // Gunakan TypedArray jika angkanya integer untuk performa lebih stabil
 
-    for(let i = 0; i < n; i++){
+    for (let i = 0; i < n; i++) {
         const val = nums[i];
-        let idx = (i + val) % n;;
-
-        if(idx < 0) idx += n
-
-        res[i] = nums[idx]
+        
+        // Menggunakan logika satu baris untuk menangani indeks negatif
+        // Ini menghindari percabangan (branching) if-else
+        let targetIdx = (i + (val % n) + n) % n;
+        
+        res[i] = nums[targetIdx];
     }
 
-    return res;
+    return Array.from(res); // Kembalikan sebagai array biasa jika kontrak fungsinya mengharuskan
 };
