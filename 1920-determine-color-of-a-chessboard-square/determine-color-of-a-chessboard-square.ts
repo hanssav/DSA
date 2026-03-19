5d05f65ccfa10d5c16201b73886779936f6007cc
Handi Irawan <63248102+hanssav@users.noreply.github.com>
2026-03-20 06:56:44 +0700

    Time: 0 ms (100.00%) | Memory: 55.5 MB (32.14%) - LeetSync

--- 1920-determine-color-of-a-chessboard-square/determine-color-of-a-chessboard-square.ts
+++ 1920-determine-color-of-a-chessboard-square/determine-color-of-a-chessboard-square.ts
@@ -1,4 +1,9 @@
 function squareIsWhite(coordinates: string): boolean {
+    // add_notes
+
+    // change word to num, a → 1, b → 2, c → 3, ..., h → 8.
+    // sum col_num + num_row; 
+    // if sum is odd (white) / true, otherwise false / black
     const w = coordinates[0].charCodeAt(0) - 96; 
     const num = parseInt(coordinates[1])
 
