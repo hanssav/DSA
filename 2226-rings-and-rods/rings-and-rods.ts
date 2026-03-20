97f60b7ca025d8d62260f4ed193a81fcad9374c2
Handi Irawan <63248102+hanssav@users.noreply.github.com>
2026-03-20 07:20:53 +0700

    Time: 0 ms (100.00%) | Memory: 54.9 MB (82.35%) - LeetSync

--- 2226-rings-and-rods/rings-and-rods.ts
+++ 2226-rings-and-rods/rings-and-rods.ts
@@ -1,18 +1,43 @@
+// function countPoints(rings: string): number {
+//     // using map;
+//     let count = new Set(); 
+//     const counts = new Map();
+
+//     for(let i = 0; i < rings.length; i += 2){
+//         const color = rings[i]; 
+//         const rod = rings[i + 1];
+//         if(count.has(rod)) continue; 
+
+//         if(!counts.has(rod)) counts.set(rod, new Set());
+//         counts.get(rod).add(color)
+
+//         if(counts.get(rod).size === 3) count.add(rod);
+//     }
+
+//     return count.size;
+// };
+
 function countPoints(rings: string): number {
-    // using map;
-    let count = new Set(); 
-    const counts = new Map();
+    // using bitmask;
+    
+    const rods = new Int32Array(10); 
 
-    for(let i = 0; i < rings.length; i += 2){
-        const color = rings[i]; 
-        const rod = rings[i + 1];
-        if(count.has(rod)) continue; 
+    for(let i = 0; i < rings.length; i+= 2){
+        const color = rings[i];
+        const idx = parseInt(rings[i + 1]);
 
-        if(!counts.has(rod)) counts.set(rod, new Set());
-        counts.get(rod).add(color)
+        let bit = 0; 
+        if(color === "R") bit = 1; 
+        else if(color === "G") bit = 2; 
+        else if(color === "B") bit = 4; 
 
-        if(counts.get(rod).size === 3) count.add(rod);
+        rods[idx] |= bit
     }
 
-    return count.size;
+    let completeRods = 0; 
+    for(let val of rods){
+        if(val === 7) completeRods++
+    }
+    
+    return completeRods
 };
