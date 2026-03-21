7cb176e8dfef2d4809527d4cd5a4ce47c1d0dff5
Handi Irawan <63248102+hanssav@users.noreply.github.com>
2026-03-21 22:21:40 +0700

    Time: 0 ms (100.00%) | Memory: 56.2 MB (53.85%) - LeetSync

--- 91-decode-ways/decode-ways.ts
+++ 91-decode-ways/decode-ways.ts
@@ -29,28 +29,59 @@
 //     return backtrack(0);
 // }
 
+// function numDecodings(s: string): number {
+//     if (!s || s[0] === '0') return 0;
+
+//     const n = s.length;
+//     const dp = new Array(n + 1).fill(0);
+    
+//     dp[0] = 1; // String kosong dianggap 1 cara dasar
+//     dp[1] = 1; // Karakter pertama pasti valid karena sudah dicek s[0] !== '0'
+
+//     for (let i = 2; i <= n; i++) {
+//         // Cek satu digit terakhir
+//         const single = parseInt(s.substring(i - 1, i));
+//         if (single >= 1 && single <= 9) {
+//             dp[i] += dp[i - 1];
+//         }
+
+//         // Cek dua digit terakhir
+//         const double = parseInt(s.substring(i - 2, i));
+//         if (double >= 10 && double <= 26) {
+//             dp[i] += dp[i - 2];
+//         }
+//     }
+
+//     return dp[n];
+// }
+
 function numDecodings(s: string): number {
     if (!s || s[0] === '0') return 0;
 
-    const n = s.length;
-    const dp = new Array(n + 1).fill(0);
-    
-    dp[0] = 1; // String kosong dianggap 1 cara dasar
-    dp[1] = 1; // Karakter pertama pasti valid karena sudah dicek s[0] !== '0'
+    let prev2 = 1; // Analog dengan dp[i-2]
+    let prev1 = 1; // Analog dengan dp[i-1]
 
-    for (let i = 2; i <= n; i++) {
-        // Cek satu digit terakhir
-        const single = parseInt(s.substring(i - 1, i));
-        if (single >= 1 && single <= 9) {
-            dp[i] += dp[i - 1];
+    for (let i = 1; i < s.length; i++) {
+        let current = 0;
+        
+        // Cek satu digit
+        if (s[i] !== '0') {
+            current += prev1;
         }
 
-        // Cek dua digit terakhir
-        const double = parseInt(s.substring(i - 2, i));
+        // Cek dua digit
+        const double = parseInt(s.substring(i - 1, i + 1));
         if (double >= 10 && double <= 26) {
-            dp[i] += dp[i - 2];
+            current += prev2;
         }
+
+        // Geser variabel untuk iterasi berikutnya
+        prev2 = prev1;
+        prev1 = current;
+        
+        // Jika di tengah jalan tidak ada cara sama sekali
+        if (current === 0) return 0;
     }
 
-    return dp[n];
+    return prev1;
 }
